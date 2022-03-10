c378c6a1afdf1d579ae42abb26303f11b90d58aa
Eyob <eyobget27@gmail.com>
2022-03-10 14:05:45 -0800

    Last commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,8 +10,7 @@ int main(void)
 	long int b;
 	long long int c;
 	char d;
-	float f;
-	
+	float f;	
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
