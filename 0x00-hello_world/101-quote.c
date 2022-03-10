f5c661736502bb7ed59a3467daed8044b02f9b25
Eyob <eyobget27@gmail.com>
2022-03-10 15:12:17 -0800

    Last commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <unistd.h>
 /**
 * main - Entry point 
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	int s;
+
+	s = strlen("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	write(2,
+  	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      s);
 	return (1);
 }
