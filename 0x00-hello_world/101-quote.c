d290eba7b4f8fdb70bb902e9e6a609b6adec7114
Eyob <eyobget27@gmail.com>
2022-03-10 14:31:33 -0800

    Last commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	writef(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
