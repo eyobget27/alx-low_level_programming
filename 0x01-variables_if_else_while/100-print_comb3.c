54397a58828238aaa8306ed9a0d27da2e267a889
Eyob <eyobget27@gmail.com>
2022-03-11 14:36:55 -0800

    Last commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,19 +11,33 @@ int main(void)
 int i;
 int j;
 
-for (i = 48; i < 57; i++)
+i = 48;
+j = 49;
+while  ((i < 57) && (j < 58))
 {
-for (j = i + 1; j < 58; j++)
 putchar(i);
 putchar(j);
-if (i != 56 || j != 57)
+if ((i == 56) && (j == 57))
 {
-putchar(',');
-putchar(' ');
+putchar('\n');
+i++;
+j++;
+}
+else
+{
+putchar(44);
+putchar(32);
+if (j < 57)
+{
+j++;
+}
+else
+{
+i++;
+j = 1 + i;
 }
 }
 }
-
-putchar (\n');
+	
 return (0);
 }
