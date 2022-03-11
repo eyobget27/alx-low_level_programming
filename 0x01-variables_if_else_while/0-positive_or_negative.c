7f978de83de7ac2f6ddbf3d1dc68a53e5148f3c0
Eyob <eyobget27@gmail.com>
2022-03-11 06:05:43 -0800

    Last commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
+nclude <stdio.h>
 
 #include <stdlib.h>
 
 #include <time.h>
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+
+   * main - Entry point
+
+    *
+
+     * Return: Always 0 (Success)
+
+      */
 
 int main(void)
 
