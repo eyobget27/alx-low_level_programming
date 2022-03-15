e4abcf18fdb0e780535000a34af56c277771ef44
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-03-16 02:10:24 +0300

    Last commit

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,1 +1,32 @@
 #include <stdio.h>
+
+/**
+ * main - print out even number in fibonacci sequence up to 4,000,000
+ *
+ * Return: (0) Success
+ */
+
+int main(void)
+{
+	int x, y, z, sum = 0;
+
+	x = 1;
+	y = 2;
+	sum = 2;
+
+	while (z <= 4000000)
+	{
+		z = x + y;
+
+		x = y;
+		y = z;
+
+		if (z % 2 == 0)
+		{
+			sum += z;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
