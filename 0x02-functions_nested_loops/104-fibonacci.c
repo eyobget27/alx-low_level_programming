0df4343f0807860229c7479e148a0ba85706da6d
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-03-16 02:13:30 +0300

    Last commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,1 +1,29 @@
 #include <stdio.h>
+
+/**
+ * main -  print the first 100 fibonacci numbers, then newline
+ *
+ * Return: (0) Success
+ */
+
+int main(void)
+{
+	int i;
+
+	unsigned long int x, y, z;
+	x = 1;
+	y = 2;
+
+	printf("%lu, %lu", x, y);
+	for (i = 0; i < 96; i++)
+	{
+		z = x + y;
+		printf(", %lu", z);
+		x = y;
+		y = z;
+	}
+
+
+	putchar('\n');
+	return (0);
+}
