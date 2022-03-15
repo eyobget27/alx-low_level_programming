26236a378c707e5b0544d1003aaf4609a6716691
Eyob <eyobget27@gmail.com>
2022-03-15 15:40:04 -0700

    Last commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main -  Print first 50 fibonacci #, followed by comma and space
+* Return: None
+*/
+int main(void)
+{
+	int i;
+
+	long int x, y, z;
+
+	x = 1;
+
+	y = 2;
+
+	printf("%ld, %ld", x, y);	
+	for (i = 0; i < 48; i++)
+	{
+		z = x + y;
+
+		printf(", %ld", z);
+		x = y;
+
+		y = z;
+
+	}
+	putchar('\n');
+	return (0);
+}
+
