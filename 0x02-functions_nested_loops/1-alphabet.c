146d8afbb1f402db1dcc0278ec050af72aeef45d
Eyob <eyobget27@gmail.com>
2022-03-15 12:42:03 -0700

    Last commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,14 @@
 
 /**
 * print_alphabet  - a function that prints the alphabet, in lowercase
-* 
 * Return: Always 0 (Success)
 */
 void print_alphabet(void)
 {
 	int n;
-	
 	for (n = 'a'; n <= 'z'; n++)
 	{
-		_putchar('n');
+		_putcha (n);
 	}
 	_putchar('\n');
 }
