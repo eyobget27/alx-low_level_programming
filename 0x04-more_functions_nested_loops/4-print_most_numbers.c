005cea221e6540e64bd079c634fd91c484084e86
Eyob <eyobget27@gmail.com>
2022-03-17 12:56:07 -0700

    Last commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,11 +7,9 @@ void print_numbers(void)
 {
 	int num;
 
-	for (num = '0'; num <= '9'; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		if (num == '2' || num == '4')
-			continue;
-		else
+		if (num != 2 || num != '4')
 			_putchar(num);
 	}
 	_putchar('\n');
