3ca3869cf014f86b1209aad39f07ea59d54ee9e0
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-03-17 23:45:48 +0300

    Last commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,28 @@
 #include "main.h"
+#include <stdio.h>
+/**
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+  int num;
+
+	for (num = 1; num < 101; num++)
+	{
+		if (num % 15 == 0)
+			printf("FizzBuzz");
+		else if (num % 3 == 0)
+			printf("Fizz");
+		else if (num % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", num);
+    if (num != 100)
+			printf(" ");
+	}
+	printf("\n");
+  
+	return (0);
+}
