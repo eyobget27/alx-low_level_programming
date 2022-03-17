5df289272a87ee924f85fe558904055ca70d3de3
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-03-17 17:57:58 +0300

    Last commit

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
 * print_remaining_days - takes a date and prints how many days are
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 100 != 0) && (year % 100 == 0))
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 	{
 		if (month >= 2 && day >= 60)
 		{
