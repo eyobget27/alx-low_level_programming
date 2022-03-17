ae54fb9f94d0e0abebd3770d12a7b0e0f64777fb
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-03-17 13:05:53 +0300

    Last commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,21 +1,15 @@
-#include "main.h"
+#include "holberton.h"
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 int main(void)
 {
-        int i;
+	int i;
 
-       if (n > 0)
-               printf("%d is positive\n", n);
-        else if (n == 0)
-                printf("%d is zero\n", n);
-        else
-                printf("%d is negative\n", n);
-        positive_or_negative(int i);
-        
-        return (0);
-}
+	i = 0;
+	positive_or_negative(i);
 
+	return (0);
         
+}  
