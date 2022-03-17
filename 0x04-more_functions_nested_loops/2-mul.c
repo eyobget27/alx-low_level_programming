99f4254b73cc08f4f55525723ab50c7b16e42ccb
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-03-17 21:50:32 +0300

    Last commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,13 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
-* mul - checks code for multiplication result
-* @a: number one
-* @b: integer two
-* Return : Always 0
+* mul - check the code for Holberton School students.
+* @a: First integer
+* @b: Second integer
+* Return: Always 0.
 */
 int mul(int a, int b)
 {
 	return (a * b);
 }
-
