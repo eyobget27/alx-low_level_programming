e1f44af42ed9b2e9fb0c2e01379a2d3802c694ca
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-03-17 12:39:50 +0300

    Last commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,26 +1,17 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
-* main - Entry point
-* Return: Always 0 (Success)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
+
 int main(void)
 {
-	int n;
+        int i;
 
-	srand(time(0));
+        i = 98;
+        positive_or_negative(i);
 
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
-
-	return (0);
+        return (0);
 }
 
+        
