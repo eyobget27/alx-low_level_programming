cf23928b29856d5223b1b8b890b0abeb6017ef6e
Eyob <eyobget27@gmail.com>
2022-03-17 11:40:01 -0700

    Last commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* mul - checks code for multiplication result
+* a: number one
+* b: number two
+* result: multiplication result
+* Return : Always 0
+*/
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	printf("%d", result);
+
+	return (0);
+}
+
