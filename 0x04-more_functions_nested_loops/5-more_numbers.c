17934a905f6b6bd81a6bd97c31abbf0194ffb624
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-03-17 23:17:24 +0300

    Last commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
-* more_numbers - checks between 0 and 14.
+* more_numbers - check the code for Holberton School students.
+*
 * Return: Always 0.
 */
 void more_numbers(void)
 {
-  int num, i;
-  
-  for (i = 0; i <= 9; i++)
-  {
-    for (num = 0; num < 15; num++)
-    {
-      if (num > 9)
-        _putchar(num / 10 + '0');
-        _putchar(num % 10 + '0');
-    }
-        _putchar('\n');
-  }
+	int num, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (num = 0; num < 15; num++)
+		{
+			if (num > 9)
+				_putchar(num / 10 + '0');
+		  _putchar(num % 10 + '0');
+		}
+	_putchar('\n');
+	}
 }
