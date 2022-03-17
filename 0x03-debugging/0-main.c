259388179abb8f7dd4d5812d1f8c412f1d01804f
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-03-17 13:22:19 +0300

    Last commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * main - Entry tests function that prints if integer is positive or negative
 * Return: Always 0 (Success)
@@ -8,7 +9,6 @@ int main(void)
 	int i;
 
 	i = 0;
-	
 	positive_or_negative(i);
 
 	return (0);
