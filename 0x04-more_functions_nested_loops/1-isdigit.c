57cd54de00c7af5745f4512ea1965d4a8cca9e36
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-03-17 21:25:38 +0300

    Last commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * _isdigit - check the code for Holberton School students.
- * @c: Integer input
- * Return: Always 0.
- */
+* _isdigit - check the code for digits between 0 and 9.
+* @c: Integer input
+* Return: Always 0.
+*/
+
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
