2b094391085afa9ad499d8dca1a61280e0f67259
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-03-17 21:27:23 +0300

    Last commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
 * _isdigit - check the code for digits between 0 and 9.
 * @c: Integer input
 * Return: Always 0.
 */
-
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
