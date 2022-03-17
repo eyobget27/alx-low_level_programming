6944313eb88f856df63938596017a5f07221da1a
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-03-17 23:01:53 +0300

    Last commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
-* print_most_numbers - checks for numbers between 0 and 9, don't print 0 & 4
-* Return: 0 Always
+* print_most_numbers - check numbers between 0 and 9.
+* Return: Always 0.
 */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int num;
 
 	for (num = '0'; num <= '9'; num++)
 	{
-		if (num != '2' || num != '4')
+		if (num == '2' || num == '4')
+			continue;
+		else
 			_putchar(num);
 	}
 	_putchar('\n');
