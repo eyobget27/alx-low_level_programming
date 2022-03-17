557c1feabf12ea8bd72cbc7a6c6265594472b10d
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-03-17 21:17:49 +0300

    Last commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,13 @@
 #include "main.h"
+/**
+ * _isupper - check the code for Holberton School students.
+ * @c: Character input
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
