b6e4f8968c84a09b48515d43b7342c0149a0bb73
Eyob <eyobget27@gmail.com>
2022-03-17 11:57:54 -0700

    Last commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*
+* print_numbers - checks code from 0 to 9
+* Return: Always 0
+*/
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		_putchar(num);
+	}
+		_putchar('\n');
+}
