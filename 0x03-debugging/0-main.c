67f1c1bdcd9f12c3fe44c0153c5fc02f0a7f4b8f
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-03-17 12:49:51 +0300

    Last commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,8 +7,12 @@ int main(void)
 {
         int i;
 
-        i = 98;
-        positive_or_negative(i);
+       if (n > 0)
+               printf("%d is positive\n", n);
+        else if (n == 0)
+                printf("%d is zero\n", n);
+        else
+                printf("%d is negative\n", n);
 
         return (0);
 }
