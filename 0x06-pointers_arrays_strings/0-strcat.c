63ec35a8859970d1c04ef994d7f7ff018d056778
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-03-24 17:33:46 +0300

    Last commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,19 +8,15 @@
 */
 char *_strcat(char *dest, char *src)
 {
-int i, j;
-	
+int i, j;	
 for (i = 0; *dest != 0; i++)
 dest++;
 for (j = 0; src[j] != 0; j++)
 {
 *dest = src[j];
-	
 dest++;
 }
 *dest = '\0';
-	
 dest -= (i + j);
-	
 return (dest);
 }
