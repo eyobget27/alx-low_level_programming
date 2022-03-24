d78bf7be931cf4e62c1f8957d75b7949b9597add
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-03-24 18:12:35 +0300

    Last commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,23 +2,21 @@
 #include <stdio.h>
 
 /**
-* reverse_array - check the code for Holberton School students.
-* @a: is a pointer type char
-* @n: is a pointer type char
-* Return: Always 0.
-*
-*/
+ * reverse_array - check the code for Holberton School students.
+ * @a: is a pointer type char
+ * @n: is a pointer type char
+ * Return: Always 0.
+ *
+ */
 void reverse_array(int *a, int n)
 {
 int result[500];
-  
 int i;
-  
 n--;
+
 for (i = 0; i <= n; i++)
 result[i] = a[i];
 
 for (i = 0; i <= n; i++)
 a[i] = result[n - i];
-  
 }
