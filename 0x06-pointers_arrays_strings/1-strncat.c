a65e9f0a9c13ba6034580cfa7cd6c0b4f6d347ad
Eyob <eyobget27@gmail.com>
2022-03-24 07:56:25 -0700

    Last commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strncat - check the code for Holberton School students.
+* @dest: is a pointer type char
+* @src: is a pointer type char
+* @n: is a int variable
+* Return: Always 0.
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	for (i = 0; *dest != 0; i++)
+		dest++;
+	for (j = 0; j < n && src[j] != 0; j++)
+	{
+		*dest = src[j];
+		dest++;
+	}
+	*dest = '\0';
+	dest -= (i + j);
+	return (dest);
+}
