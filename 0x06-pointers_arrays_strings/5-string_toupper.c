4ecde0ba24ad6145dde6a4d2013725837e9dcd80
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-03-24 18:14:33 +0300

    Last commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* string_toupper - check the code for Holberton School students.
+* @s1: is a pointer type char
+* Return: Always 0.
+*
+*/
+char *string_toupper(char *s1)
+{
+int i, k;
+  
+char l[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
+char j;
+  
+for (i = 0; s1[i] != '\0'; i++)
+{
+k = 0;
+  
+for (j = 'a'; j <= 'z'; j++, k++)
+{
+if (s1[i] == j)
+s1[i] = l[k];
+  
+}
+}
+return (s1);
+}
