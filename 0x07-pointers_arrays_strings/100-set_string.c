f95337009f35277059c8c0d29e573c8e6fcc7831
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-03-29 00:25:49 +0300

    last commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* set_string - locates a character in a string
+* @s: is a pointer of a pointer type char
+* @to: is a pointer type char
+* Return: nothing
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
