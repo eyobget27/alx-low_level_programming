9bc1aed1d73129ded07528874a896e10c22ba91f
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-04-07 19:49:18 +0300

    Last commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,61 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * string_nconcat - len of 1st str, len of 2nd str, if n < 2nd, 2nd = n
- * 2nd + 1st = total len, malloc + null byte, loop to insert into temp arr
- * @s1: input one
- * @s2: input two
- * @n: s2's number of bytes
- * Return: 0
+ * _strlen - string len
+ * @s: string
+ *
+ * Return: int len
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+/**
+ * string_nconcat - concatenates string s1 with nth of s2
+ * @s1: string 1
+ * @s2: string 2
+ * @n: size to take from s2
+ *
+ * Return: poiter to new string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *arr;
-	unsigned int i, j, co, co_2;
+	char *a;
+	unsigned int s1Len, s2Len;
+	unsigned int i, I;
+	char nul = '\0';
+	int tots;
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = &nul;
 	if (s2 == NULL)
-		s2 = "";
+		s2 = &nul;
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
+	s1Len = _strlen(s1);
+	s2Len = _strlen(s2);
 
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
+	if (n >= s2Len)
+		tots = s2Len + s1Len;
+	else
+		tots = s1Len + n;
 
-	if (n < j)
-		j = n;
-
-	j += i;
-	arr = malloc(sizeof(char *) * (j + 1));
-
-	if (arr == NULL)
+	a = malloc(tots + 1);
+	if (a == NULL)
 		return (NULL);
 
-	for (co = 0; co < i; co++)
-		arr[co] = s1[co];
-	for (co_2 = 0; co < j; co_2++)
-	{
-		arr[co] = s2[co_2];
-		co++;
-	}
-	co++;
-	arr[co] = '\0';
-	return (arr);
+	i = 0, I = 0;
+	while (s1[i] != '\0')
+		a[I++] = s1[i++];
+	i = 0;
+	while (s2[i] != '\0' && n-- != 0)
+		a[I++] = s2[i++];
+	a[I] = '\0';
+
+	return (a);
 }
