6871bb8173ef8e8c43056628c9667f2134d1fe99
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-04-11 23:44:22 +0300

    Last commit

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,14 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
+
 /**
- * free_dog - Entry Point
- * @d: struct to free
- * Return: 0
+ * free_dog - function with single argument
+ * @d: pointer type
+ *
+ * Description: frees dogs
+ * Return: na
  */
+
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
-	free(d);
+	if (d)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
 }
