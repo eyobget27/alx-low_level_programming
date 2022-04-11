7e53c75a4fb721ee0314a26cac1f9af240a19ae2
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-04-11 23:10:53 +0300

    Last commit

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
-
-#define ABS(x) (x * ((x < 0) * (-1) + (x > 0)))
-
-#endif /* FUNCTION_LIKE_MACRO */
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+#endif
