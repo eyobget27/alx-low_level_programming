9df5202f5db690bb18cc84c0c98f7ceec51ba20f
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-04-15 00:00:56 +0300

    Last commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,47 +1,101 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
- * print_all - function that prints everything
- * @format: constant pointer to constant string
- *
- * Return: void
+ * prnt_char - Prints a character pointed to by a va_list
+ * @ch: The character to be printed
+ * Return: Nothing
  */
+
+void prnt_char(va_list ch)
+{
+	printf("%c", va_arg(ch, int));
+}
+
+/**
+ * prnt_int - Prints an integer pointed to by a va_list
+ * @in: The integer to be printed
+ * Return: Nothing
+ */
+
+void prnt_int(va_list in)
+{
+	printf("%d", va_arg(in, int));
+}
+
+/**
+ * prnt_float - Prints a float pointed to by a va_list
+ * @fl: The float to be printed
+ * Return: Nothing
+ */
+
+void prnt_float(va_list fl)
+{
+	printf("%f", va_arg(fl, double));
+}
+
+/**
+ * prnt_str - Prints a string pointed to by a va_list
+ * @st: The string to be printed
+ * Return: Nothing
+ */
+
+void prnt_str(va_list st)
+{
+	char *str;
+
+	str = va_arg(st, char *);
+	switch (!str)
+	{
+	case 1:
+		printf("(nil)");
+		break;
+	default:
+		printf("%s", str);
+		break;
+	}
+}
+
+/**
+ * print_all - Function that prints anything
+ * @format: A pointer to a list of types of arguments passed to the function
+ * Return: Nothing
+ */
+
 void print_all(const char * const format, ...)
 {
-	int i, flag;
-	char *str;
-	va_list argList;
+/*i and j to step through format and chr_opt_to_prnt_opt_array */
+	int i, j;
 
-	va_start(argList, format);
+	chr_opt_to_prnt_opt_type chr_opt_to_prnt_opt_array[] = {
+		{"c", prnt_char},
+		{"i", prnt_int},
+		{"f", prnt_float},
+		{"s", prnt_str},
+		{NULL, NULL}
+	};
+
+	va_list elements_to_print;
+
+	va_start(elements_to_print, format);
 	i = 0;
-	while (format[i])
+	while (format != NULL && format[i])
 	{
-		flag = 0;
-		switch (format[i])
+		j = 0;
+		while (chr_opt_to_prnt_opt_array[j].chr_opt)
 		{
-		case ('c'):
-			printf("%c", va_arg(argList, int));
-			flag = 1;
-			break;
-		case ('i'):
-			printf("%i", va_arg(argList, int));
-			flag = 1;
-			break;
-		case ('f'):
-			printf("%f", va_arg(argList, double));
-			flag = 1;
-			break;
-		case ('s'):
-			str = va_arg(argList, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s", str);
-			flag = 1;
-			break;
+			if (format[i] == *(chr_opt_to_prnt_opt_array[j].chr_opt))
+			{
+				chr_opt_to_prnt_opt_array[j].prnt_opt(elements_to_print);
+				break;
+			}
+			j++;
 		}
-		if (format[i + 1] && flag)
+		if ((format[i + 1]) != '\0' && chr_opt_to_prnt_opt_array[j].chr_opt != NULL)
 			printf(", ");
 		i++;
 	}
+	va_end(elements_to_print);
 	printf("\n");
 }
