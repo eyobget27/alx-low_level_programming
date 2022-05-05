f8e08e817c92434e24cb4f348c1ac6aed70c1c49
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-05-05 18:31:38 +0300

    Last commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,34 @@
-/*
- * File: 1-create_file.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
-
+#include "main.h"
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
- *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * create_file - Entry Point
+ * @filename: file name
+ * @text_content: null terminated string to write
+ * Return: 1
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int file, i = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
-
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
+	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (file == -1)
 		return (-1);
 
-	close(o);
+	while (text_content[i])
+		i++;
 
+	if (text_content == NULL)
+	{
+		close(file);
+		return (-1);
+	}
+	else
+	{
+		write(file, text_content, i);
+	}
+
+	close(file);
 	return (1);
 }
