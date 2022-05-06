9d696196032dbbbe9c2a57a711175c1ad68c8250
Eyob <97969646+eyobget27@users.noreply.github.com>
2022-05-06 15:45:01 +0300

    Last commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,55 +1,24 @@
-#include "coding.h"
-#include <stdio.h>
+#include "main.h"
 
-int _pow_recursion(int x, int y);
 /**
- * binary_to_uint - function with one argument
- * @b: pointer to character
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
  *
- * Description: converts a binary number to an unsigned int
- * Return: converted number or 0 if NULL
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int value = 0;
-	unsigned int power = 0;
-	int count = 0;
+	int i;
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
-	/* Get length of string */
-	while (b[count] != '\0')
+
+	for (i = 0; b[i]; i++)
 	{
-		if (b[count] != '0' && b[count] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		count++;
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	count -= 1;
-	while (count >= 0)
-	{
-		if (b[count] == '1')
-			value += _pow_recursion(2, power);
-		power++;
-		count--;
-	}
-	return (value);
-}
 
-
-/**
- * _pow_recursion - function with two arguments
- * @x: int type 1st argument
- * @y: int type 2nd argument
- *
- * Description: returns the value of x raised to power of y
- * Return: value of x
- */
-int _pow_recursion(int x, int y)
-{
-	if (y == 0)
-		return (1);
-	else if (y < 0)
-		return (-1);
-	else
-		return (x * _pow_recursion(x, y - 1));
-}
+	return (dec_val);
